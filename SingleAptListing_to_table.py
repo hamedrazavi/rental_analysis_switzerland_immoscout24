18a27818983b838edd4246de7cfd8821fe2c5732
Hamed Razavi <srazavi@biorobpc30.epfl.ch>
2018-05-23 12:53:21 +0200

    "Fixed special cases for text to table conversion; only one thing to check "'"  was replace by '' in one single price listing which eventually turned out to be an outlier (1004 m2 surface area!, initially listed as 1'004 m2)"

--- SingleAptListing_to_table.py
+++ SingleAptListing_to_table.py
@@ -3,43 +3,32 @@ import numpy as np
 
 def SingleAptListing_to_table(single_apt_listing):
 
-    # apt_nRoom_area = single_apt_listing[2:].split("]")[0]
-    # rest = single_apt_listing[2:].split("]")[1]
-    # splt = rest[2:].split(")")
-    # apt_link = splt[0]
-    # rest = splt[1]
-    # splt = rest[6:].split("»")
-    # apt_descr = splt[0]
-    # rest = splt[1]
-    # splt = rest.splitlines()
-    # apt_address = splt[2]
-    # apt_price = splt[6][4:]
-    # apt_bkmrk = splt[-2]
-
-    apt_nRoom_area = single_apt_listing[2:].split("]")[0]
+    apt_nRoom_area = single_apt_listing.split("]")[0]
     rest = single_apt_listing[2:].split("]")[1]
     n_splt = rest[2:].find(")")
     apt_link = rest[2:][:n_splt]
     rest = rest[2:][n_splt + 1:]
-    splt = rest[6:].split("»")
-    apt_descr = ''
-    for i in range(len(splt) - 1):
-        apt_descr += '»' + splt[i]
-    rest = splt[-1]
-    splt = rest.split("Close")
-    apt_address = splt[0][2:-2]
-    apt_price = splt[1].split("Bookmark")[0][6:-2]
-    apt_bkmrk = splt[1].split("Bookmark")[1][1:-1]
+    n_splt = rest.find("»\n\n")
+    apt_descr = rest[6:n_splt]
+    rest = rest[n_splt:]
+    splt = rest.split("Close\n\n###")
+    apt_address = splt[0][3:-2]
+    rest = splt[1]
+    n_splt1 = rest.find("Bookmark")
+    n_splt2 = rest.find("##")
+    apt_price = rest[1:n_splt1 - 2]
+    apt_bkmrk = rest[n_splt1 + 10: n_splt2]
+
 
     colnames = ['Id', 'SurfaceArea', 'NumRooms', 'Type', 'Address', 'Description', 'Rent', 'Bookmark', 'Link']
     apt_df = pd.DataFrame(columns=colnames)
     apt_df['Address'] = [apt_address]
     if apt_nRoom_area.find(',') != -1:
         apt_df['NumRooms'] = [float(apt_nRoom_area.split('room')[0][0:-1])]
-        apt_df['SurfaceArea'] = [float(apt_nRoom_area.split(',')[1][1:-3])]
+        apt_df['SurfaceArea'] = [float(apt_nRoom_area.split(',')[1].replace("'", '')[1:-3])]
     elif apt_nRoom_area.find('room') == -1:
         apt_df['NumRooms'] = [1.0]
-        apt_df['SurfaceArea'] = [float(apt_nRoom_area[:-3])]
+        apt_df['SurfaceArea'] = [float(apt_nRoom_area[:-3].replace("'", ''))]
     else:
         apt_df['NumRooms'] = [float(apt_nRoom_area.split('room')[0][0:-1])]
         apt_df['SurfaceArea'] = np.nan
